92bb5d8481c45277cdf46a234829b27d0c1d8036
ShiSuYa <59110564+ShiSuYa@users.noreply.github.com>
2025-05-19 12:07:44 +0500

    Update argredir.c

--- os/lab06/argredir.c
+++ os/lab06/argredir.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <fcntl.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 
 int main(int argc, char *argv[]) {
     if (argc < 4) {
@@ -10,43 +10,57 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    int in_fd = open(argv[1], O_RDONLY);
-    if (in_fd < 0) {
+    int infd = open(argv[1], O_RDONLY);
+    if (infd < 0) {
         perror("open infile");
         exit(EXIT_FAILURE);
     }
 
-    int out_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-    if (out_fd < 0) {
+    int outfd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (outfd < 0) {
         perror("open outfile");
+        close(infd);
         exit(EXIT_FAILURE);
     }
-
-    char **cmd_args = &argv[3];
 
     pid_t pid = fork();
     if (pid < 0) {
         perror("fork");
+        close(infd);
+        close(outfd);
         exit(EXIT_FAILURE);
     }
 
     if (pid == 0) {
-        if (dup2(in_fd, STDIN_FILENO) < 0 || dup2(out_fd, STDOUT_FILENO) < 0) {
-            perror("dup2");
+        if (dup2(infd, STDIN_FILENO) < 0) {
+            perror("dup2 infile");
             exit(EXIT_FAILURE);
         }
-        close(in_fd);
-        close(out_fd);
-        execvp(cmd_args[0], cmd_args);
+
+        if (dup2(outfd, STDOUT_FILENO) < 0) {
+            perror("dup2 outfile");
+            exit(EXIT_FAILURE);
+        }
+
+        close(infd);
+        close(outfd);
+
+        execvp(argv[3], &argv[3]);
         perror("execvp");
         exit(EXIT_FAILURE);
-    } else {
-        close(in_fd);
-        close(out_fd);
-        int wstatus;
-        waitpid(pid, &wstatus, 0);
-        if (WIFEXITED(wstatus))
-            printf("WEXITSTATUS %d received\n", WEXITSTATUS(wstatus));
+    }
+
+    close(infd);
+    close(outfd);
+
+    int status;
+    if (waitpid(pid, &status, 0) < 0) {
+        perror("waitpid");
+        exit(EXIT_FAILURE);
+    }
+
+    if (WIFEXITED(status)) {
+        printf("WEXITSTATUS: %d\n", WEXITSTATUS(status));
     }
 
     return 0;
